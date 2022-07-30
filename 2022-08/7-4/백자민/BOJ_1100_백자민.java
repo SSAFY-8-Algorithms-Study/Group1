33fe39185b37d2a823f57aecb922a82fea5553b9
sdflksjf2683 <78345851+sdflksjf2683@users.noreply.github.com>
2022-07-30 22:32:16 +0900

    Create BOJ_1100_백자민.java

---
+++ 2022-08/7-4/백자민/BOJ_1100_백자민.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int cnt = 0;
+		
+		for(int i=0;i<8;i++) {
+			String s = br.readLine();
+			
+			if (i%2 == 0) {
+				for(int j=0;j<s.length();j+=2) {
+					if(s.charAt(j) == 'F') cnt++;
+				}
+			}
+			else {
+				for(int j=1;j<s.length();j+=2) {
+					if(s.charAt(j)=='F') cnt++;
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+}
