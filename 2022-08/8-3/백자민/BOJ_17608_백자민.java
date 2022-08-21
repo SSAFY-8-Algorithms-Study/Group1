0e86b2e4f702bb40d09f60de22d1190900b4538b
sdflksjf2683 <78345851+sdflksjf2683@users.noreply.github.com>
2022-08-21 20:57:48 +0900

    Add files via upload

---
+++ 2022-08/8-3/백자민/BOJ_17608_백자민.java
@@ -0,0 +1,30 @@
+package week8_3;
+
+import java.util.Scanner;
+
+public class BOJ_17608_백자민 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] bar = new int[N];
+		
+		for(int i=0;i<N;i++) {
+			bar[i] = sc.nextInt();
+		}
+		
+		int cnt = 1;
+		int height = bar[N-1];
+		for(int i=N-2;i>=0;i--) {
+			if(bar[i]>height) {
+				height = bar[i];
+				cnt++;
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+	
+}
