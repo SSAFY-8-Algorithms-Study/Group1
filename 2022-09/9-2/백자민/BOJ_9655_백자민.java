398a2da9448941a6ab6008afde6047746308f462
sdflksjf2683 <78345851+sdflksjf2683@users.noreply.github.com>
2022-09-15 22:45:47 +0900

    Add files via upload

---
+++ 2022-09/9-2/백자민/BOJ_9655_백자민.java
@@ -0,0 +1,11 @@
+package week9_2;
+
+import java.util.Scanner;
+
+public class BOJ_9655_백자민 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N%2==0?"CY":"SK");
+	}
+}
